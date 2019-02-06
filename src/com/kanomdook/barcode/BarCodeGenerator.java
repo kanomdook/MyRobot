e82357da2a993ee634814cd1c3ed7d97ecac680a
kanomdook <kanung.2535@gmail.com>
2019-02-06 14:36:33 +0700

    [add] method getBase64 form image

--- src/com/kanomdook/barcode/BarCodeGenerator.java
+++ src/com/kanomdook/barcode/BarCodeGenerator.java
@@ -1,5 +1,10 @@
 package com.kanomdook.barcode;
 
+import java.io.File;
+import java.util.Base64;
+
+import org.apache.commons.io.FileUtils;
+
 import com.keepautomation.barcode.BarCode;
 import com.keepautomation.barcode.IBarCode;
 
@@ -8,7 +13,8 @@ public class BarCodeGenerator {
 	public static void main(String[] args) {
 		try {
 //			qrCode();
-			EAN13();
+//			EAN13();
+			getBase64();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -31,7 +37,7 @@ public class BarCodeGenerator {
 
 	public static void EAN13() {
 		BarCode barcode = new BarCode();
-		barcode.setCodeToEncode("123456789999");
+		barcode.setCodeToEncode("356478366622");
 		barcode.setSymbology(IBarCode.EAN13);
 		barcode.setX(2);
 		barcode.setY(50);
@@ -42,6 +48,17 @@ public class BarCodeGenerator {
 		try {
 			barcode.draw("C:\\Users\\ASUS\\eclipse-workspace\\MyRobot\\files\\ean13.gif");
 			System.out.println("EAN13 Generate Success!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void getBase64() {
+		try {
+			File file = new File("C:\\\\Users\\\\ASUS\\\\eclipse-workspace\\\\MyRobot\\\\files\\\\ean13.gif");
+			byte[] fileContent = FileUtils.readFileToByteArray(file);
+			String encodedString = Base64.getEncoder().encodeToString(fileContent);
+			System.out.println(encodedString);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
