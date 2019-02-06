ce24629b42416251c575566932f2377028652e4a
kanomdook <kanung.2535@gmail.com>
2019-02-06 11:57:38 +0700

    [add] method generate barcode type EAN13

--- src/com/kanomdook/barcode/BarCodeGenerator.java
+++ src/com/kanomdook/barcode/BarCodeGenerator.java
@@ -7,7 +7,8 @@ public class BarCodeGenerator {
 
 	public static void main(String[] args) {
 		try {
-			qrCode();
+//			qrCode();
+			EAN13();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -23,6 +24,24 @@ public class BarCodeGenerator {
 		try {
 			barcode.draw("C:\\Users\\ASUS\\eclipse-workspace\\MyRobot\\files\\qrcode.gif");
 			System.out.println("QrCode Generate Success!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void EAN13() {
+		BarCode barcode = new BarCode();
+		barcode.setCodeToEncode("123456789999");
+		barcode.setSymbology(IBarCode.EAN13);
+		barcode.setX(2);
+		barcode.setY(50);
+		barcode.setRightMargin(0);
+		barcode.setLeftMargin(0);
+		barcode.setTopMargin(0);
+		barcode.setBottomMargin(0);
+		try {
+			barcode.draw("C:\\Users\\ASUS\\eclipse-workspace\\MyRobot\\files\\ean13.gif");
+			System.out.println("EAN13 Generate Success!");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
